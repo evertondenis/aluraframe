2e18cae7360f8baad843d8468b9a835938b6a923
Everton Denis <evertondenis@gmail.com>
2017-09-24 17:14:16 -0300

    create Proxy

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,12 +1,10 @@
-class ListaNegociacao {
-  constructor(armadilha) {
+class ListaNegociacoes {
+  constructor() {
     this._negociacoes = []
-    this._armadilha = armadilha
   }
 
   adiciona(negociacao) {
     this._negociacoes.push(negociacao)
-    this._armadilha(this)
   }
 
   get negociacoes() {
@@ -15,6 +13,5 @@ class ListaNegociacao {
 
   esvazia() {
     this._negociacoes = []
-    this._armadilha(this)
   }
 }
